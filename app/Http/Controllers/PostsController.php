200225c0e0813137bd69c0059aab1e2a2b86acc3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-05-25 23:27:21 +0900

    コントローラー作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Posts;
 
-class PostController extends Controller
+class PostsController extends Controller
 {
     public function index()
     {
-        $posts = Post::orderBy('id','Desc')->paginate(10);
+        $posts = Posts::orderBy('id','Desc')->paginate(10);
         return view('welcome',[
             'posts' => $posts,
         ]);
