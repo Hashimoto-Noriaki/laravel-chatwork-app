30ea728a56eacb596fa9fe27790de72a37ebd767
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-05-27 02:29:58 +0900

    User.phpに追記

--- app/User.php
+++ app/User.php
@@ -39,6 +39,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected $dates = [
+        'deleted_at'
+    ];
+
     public function posts(){
         return $this->hasMany(Posts::class);
     }
